2037c2ba8930fa3802cfff0707603567ff01f6ba
Gennadii <atlantagenafes@gmail.com>
2022-05-22 22:14:07 +0300

    resultCode deleted

--- src/middlewares/input-validator-middleware.ts
+++ src/middlewares/input-validator-middleware.ts
@@ -8,7 +8,6 @@ export const inputValidatorMiddleware = (req: Request, res: Response, next: Next
         next()
     } else {
         res.status(400).json({
-            resultCode: 0,
             errorsMessages: errors.array().map(e => {
                 return {
                     message: e.msg,
